2e357089f2e0077fc7cc09087f7015143c1bb7b4
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2024-02-06 11:00:05 +0600

    showing single news

--- src/app/(withNavbar)/[news]/[id]/page.jsx
+++ src/app/(withNavbar)/[news]/[id]/page.jsx
@@ -1,10 +1,89 @@
+import Comment from "@/components/UI/Comment/Comment";
+import { getSingleNews } from "@/utils/getSingleNews/getSingleNews";
+import Image from "next/image";
 import React from "react";
 
-const NewsDetailsPage = ({ params }) => {
+const NewsDetailsPage = async ({ params }) => {
+    const { data: news } = await getSingleNews(params.id);
     return (
-        <div>
-            <h2>news id: {params.id}</h2>
-        </div>
+        <section className="flex justify-center">
+            <div className="w-full max-w-[1200px] px-4 py-8 mt-10">
+                <div className="grid grid-cols-12 justify-between gap-x-12">
+                    <div className="col-span-5">
+                        <div className="grid grid-cols-12 gap-6">
+                            <div className="col-span-12">
+                                <Image
+                                    src={news.thumbnail_url}
+                                    width={800}
+                                    height={500}
+                                    alt="thumbnail"
+                                    className="w-full object-cover rounded-md"
+                                />
+                            </div>
+                            <div className="col-span-6">
+                                <Image
+                                    src={news.image_url}
+                                    width={800}
+                                    height={500}
+                                    alt="thumbnail"
+                                    className="w-full object-cover rounded-md h-full max-h-[105px]"
+                                />
+                            </div>
+                            <div className="col-span-6">
+                                <Image
+                                    src={news.thumbnail_url}
+                                    width={800}
+                                    height={500}
+                                    alt="thumbnail"
+                                    className="w-full object-cover rounded-md h-full max-h-[105px]"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-span-7">
+                        <h2 className="text-2xl font-medium text-primary opacity-85">
+                            {news.title}
+                        </h2>
+                        <div className="flex justify-start items-center gap-x-4 mt-4 mb-4">
+                            <div className="avatar">
+                                <div className="w-[35px] rounded-full">
+                                    <Image
+                                        src={news.author.img}
+                                        width={100}
+                                        height={100}
+                                        alt="avatar"
+                                    />
+                                </div>
+                            </div>
+                            <h6 className="text-[13px] font-semibold text-black opacity-75  inline-block">
+                                By {news.author.name}
+                                <span className="ml-2 text-xs">-</span>
+                                <span className="ml-2 text-xs">
+                                    {news.author.published_date}
+                                </span>
+                            </h6>
+                            {/* <span className="text-[13px] font-medium text-black opacity-75 inline-block">
+                                {news.author.published_date}
+                            </span> */}
+                        </div>
+                        <div className="mb-6">
+                            <h6 className="flex justify-start items-center text-[13px] font-medium text-gray-500">
+                                Category:
+                                <span className="ml-2 bg-gray-100 px-5 py-1 text-secondary opacity-100 rounded-lg">
+                                    {news.category}
+                                </span>
+                            </h6>
+                        </div>
+                        <div className="text-sm font-medium text-justify whitespace-pre-line text-gray-700">
+                            {news.details}
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-16 mb-8">
+                    <Comment comments={news.comments} />
+                </div>
+            </div>
+        </section>
     );
 };
 
--- src/components/UI/Comment/Comment.jsx
+++ src/components/UI/Comment/Comment.jsx
@@ -1,104 +1,105 @@
 import React from "react";
 import style from "./style.module.css";
+import Image from "next/image";
 
-const Comment = () => {
+const Comment = ({ comments }) => {
     return (
         <>
             <div class={style.commentsContainer}>
                 <h1>
-                    Comments <span className={style.fancy}>11</span>
+                    Comments
+                    <span className={style.fancy}>{comments?.length}</span>
                 </h1>
 
                 <ul id={style.commentsList} class={style.commentsList}>
                     {/* Main comment container */}
-                    <li>
-                        {/* comment main level */}
-                        <div class={style.commentMainLevel}>
-                            <div class={style.commentAvatar}>
-                                <img
-                                    src="http://i9.photobucket.com/albums/a88/creaticode/avatar_1_zps8e1c80cd.jpg"
-                                    alt=""
-                                />
-                            </div>
-
-                            <div class={style.commentBox}>
-                                <div class={style.commentHead}>
-                                    <h6 class={`${style.commentName} `}>
-                                        Agustin Ortiz
-                                    </h6>
-                                    <span>20 minutes ago</span>
-                                    <i class="fa fa-reply"></i>
-                                    <i class="fa fa-heart"></i>
-                                </div>
-                                <div class={style.commentContent}>
-                                    Lorem ipsum dolor sit amet, consectetur
-                                    adipisicing elit. Velit omnis animi et iure
-                                    laudantium vitae, praesentium optio,
-                                    sapiente distinctio illo?
-                                </div>
-                            </div>
-                        </div>
-
-                        {/* comment reply level */}
-                        <ul class={`${style.commentsList} ${style.replyList}`}>
-                            {/* reply one */}
-                            <li>
-                                <div class={style.commentAvatar}>
-                                    <img
-                                        src="http://i9.photobucket.com/albums/a88/creaticode/avatar_2_zps7de12f8b.jpg"
-                                        alt=""
-                                    />
-                                </div>
-
-                                <div class={style.commentBox}>
-                                    <div class={style.commentHead}>
-                                        <h6 class={style.commentName}>
-                                            Lorena Rojero
-                                        </h6>
-                                        <span>10 minutes ago</span>
-                                        <i class="fa fa-reply"></i>
-                                        <i class="fa fa-heart"></i>
+                    {comments?.map((comment) => {
+                        return (
+                            <li key={comment.id}>
+                                {/* comment main level */}
+                                <div class={style.commentMainLevel}>
+                                    <div class={style.commentAvatar}>
+                                        <Image
+                                            src={comment.user.img}
+                                            width={100}
+                                            height={100}
+                                            alt="avatar"
+                                            class="w-full max-w-[40px]"
+                                        />
                                     </div>
-                                    <div class={style.commentContent}>
-                                        Lorem ipsum dolor sit amet, consectetur
-                                        adipisicing elit. Velit omnis animi et
-                                        iure laudantium vitae, praesentium
-                                        optio, sapiente distinctio illo?
+
+                                    <div class={style.commentBox}>
+                                        <div class={style.commentHead}>
+                                            <h6 class={`${style.commentName} `}>
+                                                {comment.user.name}
+                                            </h6>
+                                            <span>20 minutes ago</span>
+                                            <i class="fa fa-reply"></i>
+                                            <i class="fa fa-heart"></i>
+                                        </div>
+                                        <div class={style.commentContent}>
+                                            {comment.comment}
+                                        </div>
                                     </div>
                                 </div>
+
+                                {/* comment reply level */}
+                                <ul
+                                    class={`${style.commentsList} ${style.replyList}`}
+                                >
+                                    {/* reply one */}
+                                    {comment.replies.map((reply) => {
+                                        return (
+                                            <li key={reply.id}>
+                                                <div
+                                                    class={style.commentAvatar}
+                                                >
+                                                    <Image
+                                                        src={reply.user.img}
+                                                        width={100}
+                                                        height={100}
+                                                        alt="avatar"
+                                                        class="w-full max-w-[40px]"
+                                                    />
+                                                </div>
+
+                                                <div class={style.commentBox}>
+                                                    <div
+                                                        class={
+                                                            style.commentHead
+                                                        }
+                                                    >
+                                                        <h6
+                                                            class={
+                                                                style.commentName
+                                                            }
+                                                        >
+                                                            {reply.user.name}
+                                                        </h6>
+                                                        <span>
+                                                            10 minutes ago
+                                                        </span>
+                                                        <i class="fa fa-reply"></i>
+                                                        <i class="fa fa-heart"></i>
+                                                    </div>
+                                                    <div
+                                                        class={
+                                                            style.commentContent
+                                                        }
+                                                    >
+                                                        {reply.comment}
+                                                    </div>
+                                                </div>
+                                            </li>
+                                        );
+                                    })}
+                                </ul>
                             </li>
-
-                            {/* reply two */}
-                            <li>
-                                <div class={style.commentAvatar}>
-                                    <img
-                                        src="http://i9.photobucket.com/albums/a88/creaticode/avatar_1_zps8e1c80cd.jpg"
-                                        alt=""
-                                    />
-                                </div>
-
-                                <div class={style.commentBox}>
-                                    <div class={style.commentHead}>
-                                        <h6 class={`${style.commentName}`}>
-                                            Agustin Ortiz
-                                        </h6>
-                                        <span>10 minutes ago</span>
-                                        <i class="fa fa-reply"></i>
-                                        <i class="fa fa-heart"></i>
-                                    </div>
-                                    <div class={style.commentContent}>
-                                        Lorem ipsum dolor sit amet, consectetur
-                                        adipisicing elit. Velit omnis animi et
-                                        iure laudantium vitae, praesentium
-                                        optio, sapiente distinctio illo?
-                                    </div>
-                                </div>
-                            </li>
-                        </ul>
-                    </li>
+                        );
+                    })}
 
                     {/* Main comment container */}
-                    <li>
+                    {/* <li>
                         <div class={style.commentMainLevel}>
                             <div class={style.commentAvatar}>
                                 <img
@@ -124,7 +125,7 @@ const Comment = () => {
                                 </div>
                             </div>
                         </div>
-                    </li>
+                    </li> */}
                 </ul>
             </div>
         </>
